7fe2398e0009f5d02026ad2fd6a7449400870c88
MeguminBOT <discord.meguminbot@gmail.com>
2023-10-15 12:03:28 +0200

    Optimize Song Credits popup.

--- example_mods/scripts/songname.lua
+++ example_mods/scripts/songname.lua
@@ -1,15 +1,22 @@
+function onCreate()
+	makeAnimatedLuaSprite('songnametablet', 'tablets/'..songName, -150, 290)
+	setObjectCamera('songnametablet', 'camOther')
+	addLuaSprite('songnametablet', true)
+	addAnimationByPrefix('songnametablet', 'anim', 'idle', 24, true)
+	scaleObject('songnametablet', 0.6, 0.6)
+	setProperty('songnametablet.visible', false)
+end
+
 function onStepHit()
 	if curStep == 10 then
-	makeAnimatedLuaSprite('songnametablet','tablets/'..songName, -150, 290);
-	setObjectCamera('songnametablet', 'camOther');
-	addLuaSprite('songnametablet', true);
-	addAnimationByPrefix('songnametablet','songnametablet','idle',24,true);
-	scaleObject('songnametablet', 0.6, 0.6);
-	runTimer('songnametimerdone' , 6.6);
+		playAnim('songnametablet', 'anim', true)
+		setProperty('songnametablet.visible', true)
+		runTimer('songnametimerdone', 6.6)
+	end
 end
-end
+
 function onTimerCompleted(tag)
 	if tag == "songnametimerdone" then
-	removeLuaSprite('songnametablet', true);
-end
+		removeLuaSprite('songnametablet', true)
+	end
 end
