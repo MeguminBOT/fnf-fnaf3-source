2be88c033e20fb7fe40a695e0d9b96c45d5866cb
MeguminBOT <discord.meguminbot@gmail.com>
2023-11-03 00:01:05 +0100

    Accurate rating texts

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -81,15 +81,17 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
+		['Uninstall', 0.68],
+		['Bruh', 0.69],
+		['Nice', 0.7],
+		['Shit', 0.75],
+		['Very Bad', 0.8],
+		['Bad', 0.85],
+		['OK', 0.9],
+		['Decent', 0.94],
+		['Good', 0.9625],
+		['Great', 0.98],
+		['Sick!!', 1],
 		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 
