5a2c6ad2ce2e8f0cafb27d3dd9d60911d198427f
MeguminBOT <discord.meguminbot@gmail.com>
2023-11-08 02:21:29 +0100

    Cleanup again

--- example_mods/data/notegoesaway.lua
+++
@@ -1,6 +0,0 @@
-function onCreatePost()
-	noteTweenX("NoteMove1", 0, -1500, 0.1, cubeOut)
-	noteTweenX("NoteMove2", 1, -1500, 0.1, cubeOut)
-	noteTweenX("NoteMove3", 2, -1500, 0.1, cubeOut)
-	noteTweenX("NoteMove4", 3, -1500, 0.1, cubeOut)
-end
