d12a5ffb6e92ca9767f025a97fe2b47617875b14
MeguminBOT <discord.meguminbot@gmail.com>
2024-02-17 21:33:42 +0100

    Party Room Stage/Event Optimizations

--- example_mods/custom_events/paper.lua
+++ example_mods/custom_events/paper.lua
@@ -1,15 +1,15 @@
 function onEvent(name, value1, value2)
-    if name == 'paper' then
-        makeAnimatedLuaSprite('paper','paper', 0, 0);
-        setObjectCamera('paper', 'hud')
-        addLuaSprite('paper', true);
-        addAnimationByPrefix('paper','paper','idle',24,true);
-        runTimer('gonepap', 1, 1);
-        setProperty('paper.alpha', 1);
-    end
+	if name == 'paper' then
+		runTimer('gonepap', 1, 1)
+		setProperty('paper.visible', true)
+		setProperty('paper.alpha', 1)
+		playAnim('paper', 'anim', true)
+	end
 end
-     function onTimerCompleted(tag, loops, loopsLeft)
-        if tag == 'gonepap' then
-            setProperty('paper.alpha', 0);
-    end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'gonepap' then
+		setProperty('paper.alpha', 0)
+		setProperty('paper.visible', false)
+	end
 end
--- example_mods/stages/stagepp.lua
+++ example_mods/stages/stagepp.lua
@@ -1,68 +1,101 @@
 function onCreate()
-	precacheImage('characters/paperbb');
-	precacheImage('characters/paperbonnie');
-	precacheImage('paper');
-	precacheImage('paperpeople');
-	precacheImage('paperpeople2');
-	makeLuaSprite('ppstage', 'BGs/ppstage', -2000, -550);
-	setLuaSpriteScrollFactor('ppstage', 1, 1);
-	scaleObject('ppstage', 1.07, 1.07);
-	addLuaSprite('ppstage', false);
-	makeLuaSprite('ppstage2', 'BGs/ppstage2', -750, 550);
-	setLuaSpriteScrollFactor('ppstage2', 1, 1);
-	scaleObject('ppstage2', 0.9, 0.9);
-	addLuaSprite('ppstage2', true);
+	createStage() -- Create stages.
+	createStageAnimated() -- Create animated stages.
+end
 
+function onCreatePost()
+	setProperty('paperbonnie.visible', false)
+	setProperty('paperbb.visible', false)
+end
+
+function createStage()
+	local stages = {
+		{ name = 'ppstage', image = 'BGs/ppstage', posX = -2000, posY = -550, scrollX = 1, scrollY = 1, scaleX = 1.07, scaleY = 1.07, add = false },
+		{ name = 'ppstage2', image = 'BGs/ppstage2', posX = -750, posY = 550, scrollX = 1, scrollY = 1, scaleX = 0.9, scaleY = 0.9, add = true },
+		{ name = 'ppstage3', image = 'BGs/ppstage3', posX = -750, posY = 550, scrollX = 1, scrollY = 1, scaleX = 0.9, scaleY = 0.9, add = true },
+	}
+	
+	for _, stage in ipairs(stages) do
+		precacheImage(stage.image)
+		makeLuaSprite(stage.name, stage.image, stage.posX, stage.posY)
+		setScrollFactor(stage.name, stage.scrollX, stage.scrollY)
+		scaleObject(stage.name, stage.scaleX, stage.scaleY)
+		addLuaSprite(stage.name, stage.add)
+		setProperty(stage.name .. '.visible', false)
+	end
+
+	setProperty('ppstage.visible', true)
+	setProperty('ppstage2.visible', true)
+end
+
+
+function createStageAnimated()
+	local stagesAnimated = {
+		{ name = 'bonnieanim', image = 'characters/paperbonnie', animation = 'anim', xmlPrefix = 'enter', fps = 24, loop = true, camera = 'camGame', posX = -515, posY = 240, scrollX = 1, scrollY = 1, scaleX = 1, scaleY = 1, add = false },
+		{ name = 'bbanim', image = 'characters/paperbb', animation = 'anim', xmlPrefix = 'enter', fps = 24, loop = true, camera = 'camGame', posX = -700, posY = 200, scrollX = 1, scrollY = 1, scaleX = 1, scaleY = 1, add = false },
+		{ name = 'paper', image = 'paper', animation = 'anim', xmlPrefix = 'idle', fps = 24, loop = true, camera = 'camHUD', posX = 0, posY = 0, scrollX = 1, scrollY = 1, scaleX = 1, scaleY = 1, add = true },
+		{ name = 'paperpeople', image = 'paperpeople', animation = 'anim', xmlPrefix = 'idle', fps = 24, loop = true, camera = 'camHUD', posX = -200, posY = 0, scrollX = 1, scrollY = 1, scaleX = 1, scaleY = 1, add = true },
+		{ name = 'paperpeople2', image = 'paperpeople2', animation = 'anim', xmlPrefix = 'idle', fps = 24, loop = true, camera = 'camHUD', posX = -200, posY = 600, scrollX = 1, scrollY = 1, scaleX = 1, scaleY = 1, add = true },
+	}
+
+	for _, stage in ipairs(stagesAnimated) do
+		precacheImage(stage.image)
+		makeAnimatedLuaSprite(stage.name, stage.image, stage.posX, stage.posY)
+		addAnimationByPrefix(stage.name, stage.animation, stage.xmlPrefix, stage.fps, stage.loop)
+		setScrollFactor(stage.name, stage.scrollX, stage.scrollY)
+		scaleObject(stage.name, stage.scaleX, stage.scaleY)
+
+		if (stage.camera ~= 'camGame' and immersionLevel == 'Partial') then
+			setObjectCamera(stage.name, 'camEasy')
+		else
+			setObjectCamera(stage.name, stage.camera)
+		end
+
+		addLuaSprite(stage.name, stage.add)
+		setProperty(stage.name .. '.visible', false)
+	end
 end
 
 function onStepHit()
 	if curStep == 699 then
-		makeAnimatedLuaSprite('bonnieanim', 'characters/paperbonnie', -515, 240);
-		setLuaSpriteScrollFactor('bonnieanim', 1, 1);
-		scaleObject('bonnieanim', 1, 1);
-		addAnimationByPrefix('bonnieanim','bonnieanim','enter',24,true);
-		addLuaSprite('bonnieanim', false);
+		setProperty('bonnieanim.visible', true)
+		playAnim('bonnieanim', 'anim', true)
 
-   end 
+	elseif curStep == 720 then
+		removeLuaSprite('bonnieanim', true)
+		setProperty('paperbonnie.visible', true)
 
-   if curStep == 720 then
-	removeLuaSprite('bonnieanim', true);
+		-- Clear unused memory (idk if it actually does anything meaningful, traces and profiling tools showed no real difference. I'll just leave it here, just in case)
+		runHaxeCode([[
+            Paths.clearUnusedMemory();
+        ]])
+	
+	elseif curStep == 1392 then
+		removeLuaSprite('ppstage2', true)
 
-end 
+		-- Clear unused memory (idk if it actually does anything meaningful, traces and profiling tools showed no real difference. I'll just leave it here, just in case)
+		runHaxeCode([[
+            Paths.clearUnusedMemory();
+        ]])
 
-   if curStep == 1392 then
-	removeLuaSprite('ppstage2', true);
-	makeLuaSprite('ppstage3', 'BGs/ppstage3', -750, 550);
-	setLuaSpriteScrollFactor('ppstage3', 1, 1);
-	scaleObject('ppstage3', 0.9, 0.9);
-	addLuaSprite('ppstage3', true);
-	makeAnimatedLuaSprite('bbanim', 'characters/paperbb', -700, 200);
-	setLuaSpriteScrollFactor('bbanim', 1, 1);
-	scaleObject('bbanim', 1, 1);
-	addAnimationByPrefix('bbanim','bbanim','enter',24,true);
-	addLuaSprite('bbanim', false);
+		setProperty('ppstage3.visible', true)
+		setProperty('bbanim.visible', true)
+		playAnim('bbanim', 'anim', true)
 
+	elseif curStep == 1424 then
+		removeLuaSprite('bbanim', true)
+		setProperty('paperbb.visible', true)
 
-end 
+		-- Clear unused memory (idk if it actually does anything meaningful, traces and profiling tools showed no real difference. I'll just leave it here, just in case)
+		runHaxeCode([[
+            Paths.clearUnusedMemory();
+        ]])
 
-if curStep == 1424 then
-	removeLuaSprite('bbanim', true);
-
-end 
-
-if curStep == 1936 then
-	makeAnimatedLuaSprite('paperpeople','paperpeople', -200, 0);
-	setObjectCamera('paperpeople', 'hud')
-	addLuaSprite('paperpeople', true);
-
-	addAnimationByPrefix('paperpeople','paperpeople','idle',24,true);
-
-	makeAnimatedLuaSprite('paperpeople2','paperpeople2', -200, 600);
-	setObjectCamera('paperpeople2', 'hud')
-	addLuaSprite('paperpeople2', true);
-
-	addAnimationByPrefix('paperpeople2','paperpeople2','idle',24,true);
-
-end 
-
+	elseif curStep == 1936 then
+		setProperty('paperpeople.visible', true)
+		playAnim('paperpeople', 'anim', true)
+		
+		setProperty('paperpeople2.visible', true)
+		playAnim('paperpeople2', 'anim', true)
+	end
 end
