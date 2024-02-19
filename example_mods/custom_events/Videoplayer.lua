ca6a0c8c87dd1187f0bd05484169b95cced5aa67
MeguminBOT <discord.meguminbot@gmail.com>
2024-02-19 10:57:57 +0100

    This is dumb, but it's to make sure the inCutscene variable doesn't get skipped on slower pcs or high playbackrates.

--- example_mods/custom_events/Videoplayer.lua
+++ example_mods/custom_events/Videoplayer.lua
@@ -1,8 +1,8 @@
 function onEvent(name, value1, value2)
     if name == 'Videoplayer' then
-        startVideo(value1)
         setProperty('inCutscene', false)
-
+        startVideo(value1)
+        setProperty('inCutscene', false) -- Setting a second time JUST TO MAKE SURE
     end
     return Function_Continue;
 end
