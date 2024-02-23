d3c2d3ae4b920d26f5281513d74c7350bfdaa3b1
MeguminBOT <discord.meguminbot@gmail.com>
2024-02-24 00:29:07 +0100

    Stage Optimization - Until Next Time
    - Adjusted existing subtitles.

--- example_mods/stages/stageunt.lua
+++ example_mods/stages/stageunt.lua
@@ -1,89 +1,133 @@
+-- Using Lulu's optimizations to avoid game lagging.
+
 function onCreate()
-	precacheImage('cameras');
-	precacheImage('characters/shadowbonnie');
-	precacheImage('characters/shadowbonnie2');
-	precacheImage('characters/pshadowfreddy');
-	precacheImage('characters/pshadowfreddy2');
+	-- Custom functions for stage.
+	createStage() -- Create stages.
+	createMiscSprites() -- Create misc sprites.
+end
 
-	makeLuaSprite('eyes', 'BGs/eyes', -2000, -550);
-	setScrollFactor('eyes', 1, 1);
-	scaleObject('eyes', 0.9, 0.9);
-	addLuaSprite('eyes', false);
-	setScrollFactor('eyes', 0.4, 0.4);
+function onCreatePost()
+	createSubtitles() -- Create subtitles. (Had to put them on createPost for some reason)
+end
 
-	makeLuaSprite('helloagain', 'helloagain', -400, 540);
-		setScrollFactor('helloagain', 1, 1);
-		scaleObject('helloagain', 1, 1);
-
-		addLuaSprite('helloagain', true);
-		doTweenAlpha('helloagain','helloagain', 0, 0.01, true);
-
-	makeAnimatedLuaSprite('static','static', 0, 0);
-	setObjectCamera('static', 'other')
-	addLuaSprite('static', true);
-
-	addAnimationByPrefix('static','static','idle',24,true);
-	doTweenAlpha('static','static', 0, 0.01, true);
+function createStage()
+	local stages = {
+		{ name = 'eyes', image = 'BGs/eyes', posX = -2000, posY = -550, scrollX = 0.4, scrollY = 0.4, scaleX = 0.9, scaleY = 0.9, add = false },
+		{ name = 'helloagain', image = 'BGs/helloagain', posX = -400, posY = 540, scrollX = 1, scrollY = 1, scaleX = 1, scaleY = 1, add = true },
+		{ name = 'sfedstage', image = 'BGs/sfedstage', posX = -2000, posY = -550, scrollX = 1, scrollY = 1, scaleX = 0.9, scaleY = 0.9, add = false },
+		{ name = 'untsfed', image = 'Chars/untsfed', posX = -1500, posY = 500, scrollX = 1, scrollY = 1, scaleX = 1.1, scaleY = 1.1, add = false },
+		{ name = 'sfedstage2', image = 'BGs/sfedstage2', posX = -2000, posY = -550, scrollX = 1, scrollY = 1, scaleX = 0.9, scaleY = 0.9, add = false },
+		{ name = 'cameras', image = 'BGs/cameras', posX = -500, posY = -250, scrollX = 0, scrollY = 0, scaleX = 1.9, scaleY = 1.9, add = false }
+	}
 	
-	makeLuaSprite('sfedstage', 'BGs/sfedstage', -2000, -550);
-	setScrollFactor('sfedstage', 1, 1);
-	scaleObject('sfedstage', 0.9, 0.9);
+	for _, stage in ipairs(stages) do
+		precacheImage(stage.image)
 
-	addLuaSprite('sfedstage', false);
+		if stage.name == 'cameras' then
+			makeAnimatedLuaSprite(stage.name, stage.image, stage.posX, stage.posY)
+			addAnimationByPrefix(stage.name, 'anim', 'idle', 12, true)
+		else
+			makeLuaSprite(stage.name, stage.image, stage.posX, stage.posY)
+		end
 
-	makeLuaSprite('untsfed', 'Chars/untsfed', -1500, 500);
-	setScrollFactor('untsfed', 1, 1);
-	scaleObject('untsfed', 1.1, 1.1);
+		setScrollFactor(stage.name, stage.scrollX, stage.scrollY)
+		scaleObject(stage.name, stage.scaleX, stage.scaleY)
+		addLuaSprite(stage.name, stage.add)
+		setProperty(stage.name .. '.visible', false)
+	end
 
-	addLuaSprite('untsfed', false);
+	setProperty('eyes.visible', true)
+	setProperty('sfedstage.visible', true)
+	setProperty('untsfed.visible', true)
+	setProperty('helloagain.visible', true)
+	setProperty('helloagain.alpha', 0)
+end
 
-	makeLuaSprite('black','black', 0, 0);
-        setObjectCamera('black', 'other')
-        addLuaSprite('black', true);
+function createMiscSprites()
+	local miscSprites = {
+		{ name = 'static', image = 'static', camera = 'camOther', posX = 0, posY = 0, scrollX = 1, scrollY = 1, scaleX = 1, scaleY = 1, alpha = 0 },
+		{ name = 'black', image = 'black', camera = 'camOther', posX = 0, posY = 0, scrollX = 1, scrollY = 1, scaleX = 1, scaleY = 1, alpha = 1 },
+		{ name = 'white', image = 'white', camera = 'camOther', posX = 0, posY = 0, scrollX = 1, scrollY = 1, scaleX = 1, scaleY = 1, alpha = 0 },
+		{ name = 'purple', image = 'purple', camera = 'camOther', posX = 0, posY = 0, scrollX = 1, scrollY = 1, scaleX = 1, scaleY = 1, alpha = 0 },
+		{ name = 'thegraphic', image = 'thegraphic', camera = 'camOther', posX = 0, posY = 0, scrollX = 1, scrollY = 1, scaleX = 1, scaleY = 1, alpha = 0 }
+	}
 
-		makeLuaSprite('white','white', 0, 0);
-        setObjectCamera('white', 'other')
-        addLuaSprite('white', true);
-		doTweenAlpha('white','white', 0, 0.01, true);
+	for _, miscSprite in ipairs(miscSprites) do
+		precacheImage(miscSprite.image)
 
+		if miscSprite.name == 'static' then
+			makeAnimatedLuaSprite(miscSprite.name, miscSprite.image, miscSprite.posX, miscSprite.posY)
+			addAnimationByPrefix(miscSprite.name, 'anim', 'idle', 24, true)
+		else
+			makeLuaSprite(miscSprite.name, miscSprite.image, miscSprite.posX, miscSprite.posY)
+		end
+
+		if (miscSprite.camera ~= 'camGame' and immersionLevel == 'Partial') then
+			setObjectCamera(miscSprite.name, 'camEasy')
+		else
+			setObjectCamera(miscSprite.name, miscSprite.camera)
+		end
+
+		setScrollFactor(miscSprite.name, miscSprite.scrollX, miscSprite.scrollY)
+		scaleObject(miscSprite.name, miscSprite.scaleX, miscSprite.scaleX)
+		addLuaSprite(miscSprite.name, true)
+		setProperty(miscSprite.name .. '.alpha', miscSprite.alpha)
+	end
+end
+
+function createSubtitles()
+	makeLuaSprite('TextBG', 'black', 0, 535)
+	setObjectCamera('TextBG', 'camOther')
+	scaleObject('TextBG', 1, 0.1)
+	addLuaSprite('TextBG', true)
+	setProperty('TextBG.alpha', 0)
+
+	makeLuaText('Text', '', 1200, 50, 545)
+	addLuaText('Text')
+	setTextSize('Text', 45)
+	setTextFont('Text', 'stalker2.ttf')
+	setObjectCamera('Text', 'camOther')
 end
 
 function onStepHit()
-
 	if curStep == 245 then
+		-- Remove previous stage sprites.
+		removeLuaSprite('untsfed', true)
 
-		removeLuaSprite('untsfed', true);
+		-- Clear unused memory (idk if it actually does anything meaningful, traces and profiling tools showed no real difference. I'll just leave it here, just in case)
+		runHaxeCode([[
+			Paths.clearUnusedMemory();
+		]])
+
+	elseif curStep == 1080 then
+		-- Remove previous stage sprites.
+		removeLuaSprite('sfedstage', true)
 	
-		end
+		-- Clear unused memory (idk if it actually does anything meaningful, traces and profiling tools showed no real difference. I'll just leave it here, just in case)
+		runHaxeCode([[
+			Paths.clearUnusedMemory();
+		]])
 
-		if curStep == 1080 then
+		-- Toggle visibility of stage sprites.
+		setProperty('sfedstage2.visible', true)
 
-			removeLuaSprite('sfedstage', true);
-		
-			makeLuaSprite('sfedstage2', 'BGs/sfedstage2', -2000, -550);
-			setScrollFactor('sfedstage2', 1, 1);
-			scaleObject('sfedstage2', 0.9, 0.9);
-			addLuaSprite('sfedstage2', false);
-		
-			end
+	elseif curStep == 1976 then
+		-- Remove previous stage sprites.
+		removeLuaSprite('sfedstage2', true)
+	
+		-- Clear unused memory (idk if it actually does anything meaningful, traces and profiling tools showed no real difference. I'll just leave it here, just in case)
+		runHaxeCode([[
+			Paths.clearUnusedMemory();
+		]])
 
-	if curStep == 1976 then
+		-- Toggle visibility of stage sprites.
+		setProperty('cameras.visible', true)
+		setProperty('thegraphic.visible', true)
 
-    removeLuaSprite('sfedstage2', true);
+		-- Force play animations so they start from the beginning.
+		playAnim('cameras', 'anim', true)
 
-	makeAnimatedLuaSprite('cameras', 'BGs/cameras', -500, -250);
-	setScrollFactor('cameras', 0, 0);
-	scaleObject('cameras', 1.9, 1.9);
-
-	addAnimationByPrefix('cameras','cameras','idle',12,true);
-
-	addLuaSprite('cameras', false);
-
-	makeLuaSprite('thegraphic','thegraphic', 0, 0);
-	setObjectCamera('thegraphic', 'hud')
-	addLuaSprite('thegraphic', true);
-	setProperty('thegraphic.alpha', 0)
-	setObjectOrder('thegraphic', 5)
-
+		-- Modify object order.
+		setObjectOrder('thegraphic', 5)
 	end
 end
