1f14d5d4f3047c6817e7ea7857bcaf903b56a1bf
Random people <98900217+justaguy6@users.noreply.github.com>
2024-03-02 11:31:12 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -95,7 +95,7 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !android
+		#if (android || desktop)
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
